8bc3dd2a2fe65b508b9db0c0160ccad30f5630c8
Andrew Romanov <scalli-k-be@ya.ru>
2019-03-04 11:07:14 +0700

    Initial Commit

---
+++ ResizeImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResizeImage
+//
+//  Created by Andrew Romanov on 04/03/2019.
+//  Copyright © 2019 Andrew Romanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
